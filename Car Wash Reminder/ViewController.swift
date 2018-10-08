5f098c76cbdf59f2f7992e5410a51265eebdc5b4
Hanna Östling <hannaoliviaostling@gmail.com>
2018-10-08 14:10:33 +0200

    Initial Commit

---
+++ Car Wash Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Car Wash Reminder
+//
+//  Created by Hanna Östling on 2018-10-08.
+//  Copyright © 2018 Hanna Östling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Car Wash ReminderTests/Car_Wash_ReminderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Car_Wash_ReminderTests.swift
+//  Car Wash ReminderTests
+//
+//  Created by Hanna Östling on 2018-10-08.
+//  Copyright © 2018 Hanna Östling. All rights reserved.
+//
+
+import XCTest
+@testable import Car_Wash_Reminder
+
+class Car_Wash_ReminderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
