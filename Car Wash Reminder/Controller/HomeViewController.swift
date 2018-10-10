4996041b58ef92ad36c036e91b2d0c34d6c77b7b
Hanna Östling <hannaoliviaostling@gmail.com>
2018-10-10 11:56:35 +0200

    Timeinterval for user.

--- Car Wash Reminder/Controller/HomeViewController.swift
+++ Car Wash Reminder/Controller/HomeViewController.swift
@@ -11,19 +11,24 @@ import UserNotifications
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var timeIntervalView: UIView!
     @IBOutlet weak var thumbImage: UIImageView!
     @IBOutlet weak var carWashedStatusSwitch: UISwitch!
     @IBOutlet weak var carWashedStatusLabel: UILabel!
-    
+    @IBOutlet weak var homeView: UIView!
     @IBOutlet weak var weeksPickerView: UIPickerView!
-    
+    @IBOutlet weak var selectedTimeIntervalLabel: UILabel!
     
     var logic = Logic()
-
+    var timeIntervals = ["Varje vecka", "Varannan vecka"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        logic.checkIfUserShouldWashCar()
+        //logic.checkIfUserShouldWashCar()
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in})
+        addTimeIntervals()
+        weeksPickerView.dataSource = self
+        weeksPickerView.delegate = self
     }
 
     // Test button.
@@ -51,6 +56,19 @@ class HomeViewController: UIViewController {
         }
     }
     
+    // Done button
+    @IBAction func doneButton(_ sender: Any) {
+        for i in 0...timeIntervals.count-1 {
+            if selectedTimeIntervalLabel.text == timeIntervals[i] {
+                let usersTimeIntervalInWeeks = i+1
+                logic.user.timeIntervalInWeeks = usersTimeIntervalInWeeks
+                print("Users time interval in weeks:",logic.user.timeIntervalInWeeks)
+                timeIntervalView.isHidden = true
+                homeView.isHidden = false
+            }
+        }
+    }
+    
     // Notification settings
     func sendNotification(title: String, subtitle: String, body: String) {
         let content = UNMutableNotificationContent()
@@ -63,5 +81,31 @@ class HomeViewController: UIViewController {
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
     
+    // Lägger till fler element i timeIntervals
+    func addTimeIntervals() {
+        for i in 3...12 {
+            let timeInterval = "Var \(i):e vecka"
+            timeIntervals.append(timeInterval)
+        }
+    }
+    
 
+}
+
+extension HomeViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timeIntervals.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedTimeIntervalLabel.text = timeIntervals[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return timeIntervals[row]
+    }
 }
