c0a78ab0764e2bb6aea5b94a11493150fa2f40ce
Hanna Östling <hannaoliviaostling@gmail.com>
2018-10-11 16:12:45 +0200

    Pods

---
+++ Pods/Target Support Files/Pods-Car Wash Reminder/Pods-Car Wash Reminder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Car_Wash_Reminder : NSObject
+@end
+@implementation PodsDummy_Pods_Car_Wash_Reminder
+@end
---
+++ Pods/Target Support Files/Pods-Car Wash ReminderTests/Pods-Car Wash ReminderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Car_Wash_ReminderTests : NSObject
+@end
+@implementation PodsDummy_Pods_Car_Wash_ReminderTests
+@end
