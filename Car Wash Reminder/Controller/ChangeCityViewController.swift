7d6972b663c390d889e656d9aa2ec031d960bd8c
Hanna Östling <hannaoliviaostling@gmail.com>
2018-10-12 12:17:35 +0200

    Don't need this file for now.

--- Car Wash Reminder/Controller/ChangeCityViewController.swift
+++ Car Wash Reminder/Controller/ChangeCityViewController.swift
@@ -1,33 +1,33 @@
+////
+////  ChangeCityViewController.swift
+////  Car Wash Reminder
+////
+////  Created by Hanna Östling on 2018-10-10.
+////  Copyright © 2018 Hanna Östling. All rights reserved.
+////
 //
-//  ChangeCityViewController.swift
-//  Car Wash Reminder
+//import UIKit
 //
-//  Created by Hanna Östling on 2018-10-10.
-//  Copyright © 2018 Hanna Östling. All rights reserved.
+//protocol ChangeCityDelegate {
+//    func userEnteredANewCityName(city : String)
+//}
 //
-
-import UIKit
-
-protocol ChangeCityDelegate {
-    func userEnteredANewCityName(city : String)
-}
-
-class ChangeCityViewController: UIViewController {
-    
-    var delegate : ChangeCityDelegate?
-    
-    @IBOutlet weak var changeCityTextField: UITextField!
-    
-    // Dölj status bar.
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    // Get weather button: Efter användaren skrivit in en stad och klickat på knappen så anropar vi på delegaten userEnteredANewCityName med changeCityTextField.text och går sedan tillbaka till WeatherViewController.
-    @IBAction func getWeatherPressed(_ sender: AnyObject) {
-        let cityName = changeCityTextField.text!
-        delegate?.userEnteredANewCityName(city: cityName)
-        self.navigationController?.popViewController(animated: true)
-    }
-    
-}
+//class ChangeCityViewController: UIViewController {
+//
+//    var delegate : ChangeCityDelegate?
+//
+//    @IBOutlet weak var changeCityTextField: UITextField!
+//
+//    // Dölj status bar.
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
+//
+//    // Get weather button: Efter användaren skrivit in en stad och klickat på knappen så anropar vi på delegaten userEnteredANewCityName med changeCityTextField.text och går sedan tillbaka till WeatherViewController.
+//    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+//        let cityName = changeCityTextField.text!
+//        delegate?.userEnteredANewCityName(city: cityName)
+//        self.navigationController?.popViewController(animated: true)
+//    }
+//
+//}
