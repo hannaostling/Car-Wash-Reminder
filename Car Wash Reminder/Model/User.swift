880f11c967224b67db45dfb391a56d239a6c20c9
Hanna Östling <hannaoliviaostling@gmail.com>
2018-10-15 11:03:56 +0200

    Return date in different ways.

--- Car Wash Reminder/Model/User.swift
+++ Car Wash Reminder/Model/User.swift
@@ -15,4 +15,23 @@ class User {
     var timeIntervalChoiseIsMade: Bool = false
     let car = Car()
     
+    // Returnerar en sträng av startSearchingDate
+    func dateWithFormatter() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        let date = formatter.string(from: startSearchingDate)
+        return date
+    }
+    
+    // Returnerar ett heltal hur många dagar det är kvar tills appen börjar leta efter ett datum igen.
+    func howManyDaysToSearchingDate() -> Int {
+        let calendar = Calendar.current
+        let date1 = calendar.startOfDay(for: Date())
+        let date2 = calendar.startOfDay(for: startSearchingDate)
+        let components = calendar.dateComponents([.day], from: date1, to: date2)
+        let daysLeft = components.day!
+        return daysLeft
+    }
+    
+    
 }
