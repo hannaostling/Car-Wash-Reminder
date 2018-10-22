309146512dd168c4b0d5de9102ccdc63220ba59c
Hanna Östling <hannaoliviaostling@gmail.com>
2018-10-22 13:43:58 +0200

    Better alert message + alert in viewDidLoad (before: only when user press info button).

--- Car Wash Reminder/Controller/TimeIntervalViewController.swift
+++ Car Wash Reminder/Controller/TimeIntervalViewController.swift
@@ -23,6 +23,7 @@ class TimeIntervalViewController: UIViewController {
         addTimeIntervals()
         weeksPickerView.dataSource = self
         weeksPickerView.delegate = self
+        giveInformationAlert()
     }
     
     // När använvaren väljer tidsintervall och klickar på "klar" så sparas tidsintervallet som ett heltal i logis.user.timeIntervalInWeeks.
@@ -42,11 +43,7 @@ class TimeIntervalViewController: UIViewController {
     // Om användaren inte förstår vad det är för tidsintervall så kan man klicka på info för att få mer information.
     @IBAction func infoButtonPressed(_ sender: Any) {
         print("Info button pressed!")
-        let title = "Information"
-        let message = "Du ska välja ett tidsintervall på den tid du vill att appen ska ta en paus från att leta efter ett bra datum att tvätta bilen, när du markerat din bil som tvättad."
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "Okej", style: UIAlertAction.Style.default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        giveInformationAlert()
     }
     
     // Lägger till fler element i timeIntervals.
@@ -55,6 +52,15 @@ class TimeIntervalViewController: UIViewController {
             let timeInterval = "Var \(i):e vecka"
             timeIntervals.append(timeInterval)
         }
+    }
+    
+    // Alert med information till användaren.
+    func giveInformationAlert() {
+        let title = "Information"
+        let message = "Appen söker efter en bra dag att tvätta din bil med bakgrundsdata och du får en notis när det är bra läge att tvätta bilen. \n \n När du sedan tvättat din bil kan du markera din bil som tvättad. Då kommer appen ta en paus från att söka efter en bra dag att tvätta bilen. \n \n Hur lång paus vill du att appen ska låta bli att leta efter en bra dag att tvätta bilen? Det tidsintervallet väljer du här."
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Okej", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 
 }
