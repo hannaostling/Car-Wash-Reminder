f48097ddfbe07a0e1e70593172c4d4bb72f58f75
Hanna Östling <hannaoliviaostling@gmail.com>
2018-10-25 23:21:01 +0200

    Start searching date in car, not user

--- Car Wash Reminder/Model/Car.swift
+++ Car Wash Reminder/Model/Car.swift
@@ -11,6 +11,7 @@ import Foundation
 class Car {
 
     var name: String = "Bilen"
+    var startSearchingDate = Date()
     var isDirtyBool: Bool = true
     var isDirtyDate = Date()
     var washedDates: [Date] = []
@@ -19,6 +20,7 @@ class Car {
     
     // Dictionary nycklar
     let carName = "carName"
+    let carSearchingDate = "carSearchingDate"
     let carIsDirtyBool = "carIsDirtyBool"
     let carIsDirtyDate = "carIsDirtyDate"
     let carWashedDates = "carWashedDates"
@@ -28,6 +30,7 @@ class Car {
     
     init(dataDictionary:[String:Any]) {
         name = dataDictionary[carName] as! String
+        startSearchingDate = dataDictionary[carSearchingDate] as! Date
         isDirtyBool = dataDictionary[carIsDirtyBool] as! Bool
         isDirtyDate = dataDictionary[carIsDirtyDate] as! Date
         washedDates = dataDictionary[carWashedDates] as! [Date]
@@ -37,6 +40,7 @@ class Car {
     func dataDictionaryFromObject() -> [String:Any] {
         var dictionary = [String:Any]()
         dictionary[carName] = name
+        dictionary[carSearchingDate] = startSearchingDate
         dictionary[carIsDirtyBool] = isDirtyBool
         dictionary[carIsDirtyDate] = isDirtyDate
         dictionary[carWashedDates] = washedDates
@@ -53,5 +57,25 @@ class Car {
         let daysLeft = components.day!
         return daysLeft
     }
+    
+    // Börja söka igen efter bilens tidsinterval.
+    func startSearchingAgainAfter(timeInterval: Int) -> Date {
+        let daysToAdd = 7 * timeInterval
+        let calendar = Calendar.current
+        let currentDate = Date()
+        startSearchingDate = calendar.date(byAdding: .day, value: daysToAdd, to: currentDate)!
+        let carNotWashedRecently = daysToAdd - 6
+        isDirtyDate = calendar.date(byAdding: .day, value: carNotWashedRecently, to: currentDate)!
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        let currentDateInString = formatter.string(from: Date())
+        let carIsDirtyDate = formatter.string(from: isDirtyDate)
+        let startSearchingDateInString = formatter.string(from: startSearchingDate)
+        print("Dagens datum: \(currentDateInString)")
+        print("Bilen räknas som smutsig igen: \(carIsDirtyDate)")
+        print("Appen börjar leta efter bra datum: \(startSearchingDateInString)")
+        return startSearchingDate
+    }
+    
 
 }
--- Car Wash Reminder/Model/User.swift
+++ Car Wash Reminder/Model/User.swift
@@ -11,7 +11,6 @@ import UIKit
 
 class User {
 
-    var startSearchingDate = Date()
     var hasOpenedAppBefore: Bool = false
     var lastSearchedCity: String = ""
     var lastPositionCity: String = ""
@@ -19,43 +18,6 @@ class User {
     var cityParams = ["":""]
     var chosenCarIndex = 0
     let carObject = Car()
-        
-    // Returnerar en sträng av startSearchingDate
-    func searchAgainDateInString() -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd/MM/yyyy hh:mm:ss"
-        let date = formatter.string(from: startSearchingDate)
-        return date
-    }
-    
-    // Returnerar ett heltal hur många dagar det är kvar tills appen börjar leta efter ett datum igen.
-    func howManyDaysToSearchingDate() -> Int {
-        let calendar = Calendar.current
-        let date1 = calendar.startOfDay(for: Date())
-        let date2 = calendar.startOfDay(for: startSearchingDate)
-        let components = calendar.dateComponents([.day], from: date1, to: date2)
-        let daysLeft = components.day!
-        return daysLeft
-    }
-    
-    // Börja söka igen efter användarens tidsinterval.
-    func startSearchingAgainAfter(timeInterval: Int) {
-        let daysToAdd = 7 * timeInterval
-        let calendar = Calendar.current
-        let currentDate = Date()
-        startSearchingDate = calendar.date(byAdding: .day, value: daysToAdd, to: currentDate)!
-        let carNotWashedRecently = daysToAdd - 6
-        var isDirtyDate = carObject.carDataDictionaryArray[chosenCarIndex][carObject.carIsDirtyDate] as! Date
-        isDirtyDate = calendar.date(byAdding: .day, value: carNotWashedRecently, to: currentDate)!
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd/MM/yyyy"
-        let currentDateInString = formatter.string(from: Date())
-        let carIsDirtyDate = formatter.string(from: isDirtyDate)
-        let startSearchingDateInString = formatter.string(from: startSearchingDate)
-        print("Dagens datum: \(currentDateInString)")
-        print("Bilen räknas som smutsig igen: \(carIsDirtyDate)")
-        print("Appen börjar leta efter bra datum: \(startSearchingDateInString)")
-    }
     
     func checkUserStatus() -> UserStatus {
         let amountOfCars = carObject.carDataDictionaryArray.count
